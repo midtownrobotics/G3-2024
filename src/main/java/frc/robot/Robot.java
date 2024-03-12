040a98e5b324bcf660dafc5f345fa781b462f922
Gray <me@grayjn.com>
2024-03-12 14:47:00 -0400

    finished intake rumble (needs testing)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-// import edu.wpi.first.net.PortForwarder;
-import frc.robot.commands.IntakeBeamBreak;
+import frc.robot.subsystems.IntakeBeamBreak;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -21,8 +20,11 @@ public class Robot extends TimedRobot {
 	private Command m_autonomousCommand;
 
 	private RobotContainer m_robotContainer;
+	private IntakeBeamBreak m_intakeBeamBreak;
 	public boolean noteSensorBoolean;
 	private boolean noteSensorBooleanLast = false;
+
+	private int timer;
 
 	/**
 	 * This function is run when the robot is first started up and should be used for any
@@ -42,6 +44,7 @@ public class Robot extends TimedRobot {
 		// Instantiate our RobotContainer.  This will perform all our button bindings, and put our
 		// autonomous chooser on the dashboard.
 		m_robotContainer = new RobotContainer();
+		m_intakeBeamBreak = new IntakeBeamBreak();
 
 		SmartDashboard.putData("Swerve Odometry", m_robotContainer.getField());		
 	}
@@ -62,13 +65,20 @@ public class Robot extends TimedRobot {
 		// m_robotContainer.getDistanceThing();
 		CommandScheduler.getInstance().run();
 
+		timer ++;
+
 		noteSensorBoolean = m_robotContainer.getIntake().getNoteSensor();
 
 		if (noteSensorBoolean != noteSensorBooleanLast) {
 			noteSensorBooleanLast = noteSensorBoolean;
 			if (noteSensorBoolean) {
-				new IntakeBeamBreak().onTrue();
+				m_intakeBeamBreak.onTrue();
+				timer = 0;
 			}
+		}
+
+		if (timer == 1 * 1000 / 20) {
+			m_intakeBeamBreak.stop();
 		}
 
 	}
