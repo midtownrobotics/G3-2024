c4e8ec07fb86d80a176e1e549e0638f0477c7446
Gray486 <me@grayjn.com>
2024-03-16 10:42:11 -0400

    stopped redefining xbox controller every time intakeBeamBreak is run

--- src/main/java/frc/robot/subsystems/IntakeBeamBreak.java
+++ src/main/java/frc/robot/subsystems/IntakeBeamBreak.java
@@ -5,20 +5,18 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Ports;
 
 public class IntakeBeamBreak {
-    XboxController driver;
+    private final XboxController driver;
 
     public IntakeBeamBreak() {
         this.driver = new XboxController(Ports.USB.DRIVER_CONTROLLER);
     }
 
     public void onTrue() {
-        XboxController driver = new XboxController(Ports.USB.DRIVER_CONTROLLER);
         driver.setRumble(GenericHID.RumbleType.kRightRumble, 1);
         driver.setRumble(GenericHID.RumbleType.kLeftRumble, 1);
     }
 
     public void stop() {
-        XboxController driver = new XboxController(Ports.USB.DRIVER_CONTROLLER);
         driver.setRumble(GenericHID.RumbleType.kRightRumble, 0);
         driver.setRumble(GenericHID.RumbleType.kLeftRumble, 0);
     }
