0d622b644fa539170086d3dde328a871f55e6c8a
Gray486 <me@grayjn.com>
2024-03-16 16:03:37 -0400

    added fixed everything

---
+++ src/main/java/frc/robot/subsystems/IntakeBeamBreak.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Ports;
+
+public class IntakeBeamBreak {
+    private final XboxController driver;
+
+    public IntakeBeamBreak() {
+        this.driver = new XboxController(Ports.USB.DRIVER_CONTROLLER);
+    }
+
+    public void onTrue() {
+        driver.setRumble(GenericHID.RumbleType.kRightRumble, 1);
+        driver.setRumble(GenericHID.RumbleType.kLeftRumble, 1);
+    }
+
+    public void stop() {
+        driver.setRumble(GenericHID.RumbleType.kRightRumble, 0);
+        driver.setRumble(GenericHID.RumbleType.kLeftRumble, 0);
+    }
+}
